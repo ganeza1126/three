e42216656d3294b356656922cfb41e1ac8dd871e
ganeza1126 <ganeza1126@g.ecc.u-tokyo.ac.jp>
2020-06-09 21:04:51 +0900

    smoke perspective

--- main/js/proto.js
+++ main/js/proto.js
@@ -8,16 +8,18 @@
 
 	//new-start
 	var loader = new THREE.TextureLoader();
-	//var smokeTexture = loader.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
-	var smokeTexture = loader.load('../Smoke-Element.png');
+	var smokeTexture = loader.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
+	//	var smokeTexture = loader.load('Smoke-Element.png');
 	var smokeGroup = new THREE.Group();
 
 	//回転と移動用の変数（とりあえずのものなので動きがわかったら使わなくてOKです）
-	var c = 0;
 	var clst = [];
 	//new-end
 
 	var frame, walls, axes;
+
+	var rad = 0.5;//半径
+	var c = 0;// 微小変化量
 
 	$(function () {
 
@@ -37,16 +39,25 @@
 		//newstart
 		//smokeGeo = new THREE.PlaneGeometry(1, 1);
 		smokeGeo = new THREE.SphereGeometry(1);
+		/*
 		smokeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, map: smokeTexture, transparent: true, opacity: 0.8 });
 		smokeMaterial2 = new THREE.MeshLambertMaterial({ color: 0xff0000, map: smokeTexture, transparent: true, opacity: 0.8 });
 		smokeMaterial3 = new THREE.MeshBasicMaterial({ color: 0x00ff00, map: smokeTexture, transparent: true, opacity: 0.8 });
 		smokeMaterial4 = new THREE.MeshBasicMaterial({ color: 0xffff00, map: smokeTexture, transparent: true, opacity: 0.8 });
 		smokeMaterial5 = new THREE.MeshBasicMaterial({ color: 0x0000ff, map: smokeTexture, transparent: true, opacity: 0.8 });
-		smokeParticles = [];
-		//sphereを50個生成		
-		for (var i = 0; i < 50; i++) {
-			smokeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, map: smokeTexture, transparent: true, opacity: 0.8 });
+		*/
 
+		smokeParticles = [];
+
+
+		//sphereを50個生成
+		for (var i = 0; i < 100; i++) {
+			//	var sphere = new THREE.Mesh(smokeGeo, smokeMaterial);
+
+			//smokeMaterial = new THREE.SpriteMaterial({ color: 0x0000ff, map: smokeTexture, transparent: true, opacity: 0.4 });
+			smokeMaterial = new THREE.SpriteMaterial({ color: randcolor(), map: smokeTexture, transparent: true, opacity: 0.2 });
+			var sphere = new THREE.Sprite(smokeMaterial);
+			/*
 			switch (i % 5) {
 				case 0:
 					var sphere = new THREE.Mesh(smokeGeo, smokeMaterial);
@@ -63,21 +74,22 @@
 				case 4:
 					var sphere = new THREE.Mesh(smokeGeo, smokeMaterial4);
 					break;
-			}
-			//			if (i % 5 == 0) {
-			//				var sphere = new THREE.Mesh(smokeGeo, smokeMaterial);
-			//			}
-			//			else { var sphere = new THREE.Mesh(smokeGeo, smokeMaterial); }
-			//sphereの半径を1にして、色を赤に
-			//			var sphere = new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ transparent: true, map: smokeTexture, opacity: 0.8, color: 0xff0000 }));
-			//sphereの半径が小さいバージョン（0.1）
-			//			var sphere = new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({ transparent: true, map: smokeTexture, opacity: 0.8, color: 0xff0000 }));
+			*/
+
 			//sphereの初期の回転角度を持たせておく
-			sphere.ry = Math.random() * Math.PI * 2;
+			//sphere.ry = Math.random() * Math.PI * 2;
 			//y軸回転をその角度にする
-			sphere.rotation.set(0, sphere.ry, 0);
+			//sphere.rotation.set(0, sphere.ry, 0);
 			//座標の範囲を-0.5〜0.5に狭める
-			sphere.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+			//			sphere.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+			var z = (Math.random() - 0.5) * Math.PI;
+			var th = Math.random() * 2 * Math.PI;
+			var r = Math.random() * rad;
+			sphere.position.set(r * Math.sin(z) * Math.cos(th), r * Math.sin(z) * Math.sin(th), r * Math.cos(z))
+
+
+
+
 			smokeGroup.add(sphere);
 			scene.add(smokeGroup);
 			smokeParticles.push(sphere);
@@ -104,6 +116,9 @@
 
 	});
 
+	function randcolor() {
+		return '#' + Math.floor(Math.random() * 1677215).toString(16);
+	}
 	//画面いっぱいに#mainを表示するための関数
 	function setMain() {
 		//画面の幅と高さを取得して
@@ -128,7 +143,6 @@
 		//			var child = smokeGroup.children[i];
 		//			child.rotation.set(0, child.ry + c * 3, 0);
 		//		}
-		var sp = smokeParticles.length;
 		//		//rei1
 		//		c = 0.001;
 		//		while (sp--) {
@@ -138,11 +152,29 @@
 		//			//	smokeParticles[sp].position.x += (Math.random() - 0.5) * c;
 		//		}
 		//rei2
+		var sp = smokeParticles.length;
+		c = rad * 0.01;
 		while (sp--) {
-			smokeParticles[sp].rotation.x += 0.0051 * clst[sp];
-			smokeParticles[sp].rotation.y += 0.0051 * clst[sp];
-			smokeParticles[sp].rotation.z += 0.0051 * clst[sp];
-			//	smokeParticles[sp].position.x += (Math.random() - 0.5) * c;
+			var rdx = (Math.random() - 0.5) * c;
+			var rdy = (Math.random() - 0.5) * c;
+			var rdz = (Math.random() - 0.5) * c;
+			var pos = smokeParticles[sp].position;
+			pos.x += rdx;
+			pos.y += rdy;
+			pos.z += rdz;
+			var aaa = Math.pow(pos.x, 2) + Math.pow(pos.y, 2) + Math.pow(pos.z, 2);
+			if (aaa > rad * rad) {
+				pos.x -= rdx * 2;
+				pos.y -= rdy * 2;
+				pos.z -= rdz * 2;
+			}
+
+			//			smokeParticles[sp].rotation.x += 0.0051 * clst[sp];
+			//			smokeParticles[sp].rotation.y += 0.0051 * clst[sp];
+			//			smokeParticles[sp].rotation.z += 0.0051 * clst[sp];
+			//			smokeParticles[sp].position.x = smokeParticles[sp].position.x + ((Math.random() - 0.5) * c);
+			//		smokeParticles[sp].position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+
 		}
 
 		//smokeGroupの座標をcの分だけ上昇させる
@@ -157,6 +189,24 @@
 		requestAnimationFrame(update);
 	}
 
+	function MakeBot() {
+
+		for (var i = 0; i < 100; i++) {
+			smokeMaterial = new THREE.SpriteMaterial({ color: randcolor(), map: smokeTexture, transparent: true, opacity: 0.2 });
+			var sphere = new THREE.Sprite(smokeMaterial);
+			var z = (Math.random() - 0.5) * Math.PI;
+			var th = Math.random() * 2 * Math.PI;
+			var r = Math.random() * rad;
+			sphere.position.set(r * Math.sin(z) * Math.cos(th), r * Math.sin(z) * Math.sin(th), r * Math.cos(z))
+
+			smokeGroup.add(sphere);
+			scene.add(smokeGroup);
+			smokeParticles.push(sphere);
+		}
+
+	}
+
+	/*
 	function getRot() {
 		var camPos = camera.position;
 		var mePos = me.position;
@@ -191,7 +241,44 @@
 
 	}
 
+	*/
 
+	/*
+	function tick() { //毎フレーム実行する動作
+		requestAnimationFrame(tick);
+
+		for (i = 0; i < bot_cnt; i++) {
+			// botを動かす
+			bot_direction[i][1] += (bot_direction[i][2] - bot_direction[i][0]) / bot_directionchange_freq;
+			if (cnt == 0) {
+				bot_direction[i][0] = bot_direction[i][1];
+				bot_direction[i][2] = 2 * (Math.PI) * (Math.random());
+			};
+			bot[i].position.x += bot_speed * Math.cos(bot_direction[i][1]);
+			bot[i].position.z += bot_speed * Math.sin(bot_direction[i][1]);
+
+			// 壁に当たった時
+			if (Math.abs(bot[i].position.x) >= ground_width / 2) {
+				bot_direction[i][1] = Math.PI - bot_direction[i][1];
+				bot_direction[i][0] = Math.PI - bot_direction[i][0];
+			};
+			if (Math.abs(bot[i].position.z) >= ground_depth / 2) {
+				bot_direction[i][1] = -bot_direction[i][1];
+				bot_direction[i][0] = -bot_direction[i][0];
+			};
+		};
+
+		// レンダリング
+		renderer.render(scene, camera);
+
+		// カウンター更新
+		if (cnt == bot_directionchange_freq) {
+			cnt = 0;
+		} else {
+			cnt += 1;
+		};
+	};
+	*/
 
 
 })();
