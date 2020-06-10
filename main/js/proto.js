158a0e785368fb96b2b365598952121fb0d85bc7
ganeza1126 <ganeza1126@g.ecc.u-tokyo.ac.jp>
2020-06-11 03:36:33 +0900

    swap color not using bounding sphere and add description comment

--- main/js/proto.js
+++ main/js/proto.js
@@ -1,37 +1,29 @@
 (function () {
-
-	//画面の幅と高さ
+	//-------------------------------------------------------
 	var W, H;
-	//Three.js（3D表示用のjs）のシーン、レンダラ、カメラ
 	var scene = new THREE.Scene(), renderer = new THREE.WebGLRenderer({ alpha: true }), camera, controls;
 	renderer.setClearColor(0x000000, 0.5);
-
-	//new-start
-	var loader = new THREE.TextureLoader();
-	var smokeTexture = loader.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
-
-
-
 	var frame, walls, axes;
+	var loader = new THREE.TextureLoader();
+	const ground_x = 8;//赤軸方向のサイズ
+	const ground_y = 1;//緑軸方向のサイズ
+	const ground_z = 8;//青軸方向のサイズ
+	//-------------------------------------------------------
 
-
-	var bot = [];
-	var bot_direction = [];
-	const bot_directionchange_freq = 150;
-	//const bot_speed = 0.01;
-	const bot_speed = 0.005;
-	const ground_x = 8;
-	const ground_y = 1;
-	const ground_z = 8;
-	const bot_rad = 0.5;//半径
-	const smoke_num = 200;
+	//bot関連
 	const bot_num = 3;
-	const smoke_speed = 0.01;// 微小変化量（煙の）
-	var cnt = 0;
-	var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x000000, transparent: true, opacity: 0 });
-
-
-	var bot_pos = [];//ボット間の距離を測る
+	var bot = [];//bot_numの数の格納
+	var bot_pos = [];//ボットの位置を格納
+	const bot_speed = 0.01;
+	const bot_rad = 0.5;//ボットの半径
+	var bot_direction = [];//ボットの進行方向？森田さんのBotMov関数
+	const bot_directionchange_freq = 150;//なんかの周波数森田さんのBotMov関数
+	var cnt = 0;//なんかのカウンタ，森田さんのBotMov関数
+	//smoke関連
+	const smoke_num = 200;//各ボットの煙の数
+	const smoke_speed = 0.05;//煙のゆらぎ？の速度
+	const smoke_opacity = 0.05;//煙の透明度
+	const smokeTexture = loader.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');//煙の画像
 
 	$(function () {
 		//-------------------------------------------------------
@@ -50,42 +42,48 @@
 		scene.add(frame);
 		//-------------------------------------------------------
 
+		//bot作成(switchとMakebotの中身は検証用に変更中)
 		for (var i = 0; i < bot_num; i++) {
-			MakeBot();
+			var basecolor;
+			switch (i) {
+				case 0: basecolor = 0xFF0000; break;
+				case 1: basecolor = 0x00FF00; break;
+				case 2: basecolor = 0x0000FF; break;
+				default: basecolor = randcolor();
+			}
+			MakeBot(basecolor);
 		}
-		var botid = bot.length;
-
-
-		//			console.log(bot[botid].children[bot_num].geometry.BoundingSphere);
 
 		update();
-
 	});
 
-	//画面いっぱいに#mainを表示するための関数
-	function setMain() {
-		//画面の幅と高さを取得して
-		W = $("#main").width();
-		H = $(window).height();
-		//#mainに設定
-		$("#main").height(H);
-		//3D空間の大きさを画面の幅と高さに設定
-		renderer.setSize(W, H);
-		if (camera) {
-			//カメラの画角も画面サイズにあったものに
-			camera.aspect = W / H;
-			camera.updateProjectionMatrix();
-		}
-	}
 
-	//繰り返し実行するupdate()
+	//繰り返し実行する
 	function update() {
-		var botid = bot.length;
+		var botid = bot_num;
 		while (botid--) {
 			SmokeMov(bot[botid]);
 			BotMov(botid);
-			bot_pos[botid] = [bot[botid].position.x, bot[botid].position.z]
+			bot_pos[botid] = [bot[botid].position.x, bot[botid].position.z];//ボットの平面の位置を代入
 		}
+		SwapSmoke();
+
+		// botmov用カウンター更新
+		if (cnt == bot_directionchange_freq) {
+			cnt = 0;
+		} else {
+			cnt += 1;
+		}
+
+		//-------------------------------------------------------
+		controls.update();
+		renderer.render(scene, camera);
+		requestAnimationFrame(update);
+		//-------------------------------------------------------
+	}
+
+	//煙の色交換
+	function SwapSmoke() {
 		var i = bot_num;
 		while (i--) {
 			var j = i;
@@ -95,65 +93,15 @@
 				if (Math.pow(x[0] - y[0], 2) + Math.pow(x[1] - y[1], 2) <= bot_rad * bot_rad) {
 					var colorid = Math.floor(Math.random() * smoke_num);
 					var tmp = bot[i].children[colorid].material.color;
-					//console.log("hit!!!!!!!!", tmp);
 					bot[i].children[colorid].material.color = bot[j].children[colorid].material.color;
 					bot[j].children[colorid].material.color = tmp;
 				}
 			}
 		}
-		//console.log(bot_pos)
-
-		//add bounding sphereを使う場合
-		//console.log(bot[0].children[smoke_num].geometry.boundingSphere.intersectsSphere(bot[1].children[smoke_num].geometry.boundingSphere));
-		//add
-		// botmov用カウンター更新
-		if (cnt == bot_directionchange_freq) {
-			cnt = 0;
-		} else {
-			cnt += 1;
-		};
-
-		//-------------------------------------------------------
-		controls.update();
-		renderer.render(scene, camera);
-		requestAnimationFrame(update);
-		//-------------------------------------------------------
 	}
 
 
-	//function MakeBot(basecolor = randcolor()) {
-	function MakeBot(basecolor = randcolor()) {
-		var smokeParticles = new THREE.Group();
-		for (var i = 0; i < smoke_num; i++) {
-			//単色
-			var smokeMaterial = new THREE.SpriteMaterial({ color: basecolor, map: smokeTexture, transparent: true, opacity: 0.2 });
-			//混色
-			//var smokeMaterial = new THREE.SpriteMaterial({ color: randcolor(), map: smokeTexture, transparent: true, opacity: 0.2 });
-			var sphere = new THREE.Sprite(smokeMaterial);
-			var z = (Math.random() - 0.5) * Math.PI;
-			var th = Math.random() * 2 * Math.PI;
-			var r = Math.random() * bot_rad;
-			sphere.position.set(r * Math.sin(z) * Math.cos(th), r * Math.sin(z) * Math.sin(th), r * Math.cos(z))
-			smokeParticles.add(sphere);
-		}
-		//boundingsphereを使う場合
-		////add
-		//var sphere = new THREE.Mesh(new THREE.SphereGeometry(bot_rad), sphereMaterial)
-		//sphere.geometry.computeBoundingSphere();
-		//smokeParticles.add(sphere);
-		////add
-
-		//otamesi
-		//console.log(smokeParticles.children[0].material.color);
-		//otamesi
-
-		scene.add(smokeParticles);
-		bot.push(smokeParticles);
-		bot_direction.push([0, 0, Math.PI / 2]);
-
-	}
-
-	//煙を動かす
+	//煙を動かすver1
 	function SmokeMov(smokeParticles) {
 		var sp = smokeParticles.children.length;
 		while (sp--) {
@@ -174,8 +122,8 @@
 	}
 
 
-
-	function BotMov(i) { //毎フレーム実行する動作
+	//森田さんの関数
+	function BotMov(i) {
 		// botを動かす
 		bot_direction[i][1] += (bot_direction[i][2] - bot_direction[i][0]) / bot_directionchange_freq;
 		if (cnt == 0) {
@@ -186,24 +134,60 @@
 		bot[i].position.z += bot_speed * Math.sin(bot_direction[i][1]);
 
 		// 壁に当たった時
-		if (Math.abs(bot[i].position.x) >= ground_x / 2) {
+		if (Math.abs(bot[i].position.x) >= ground_x / 2.0) {
 			bot_direction[i][1] = Math.PI - bot_direction[i][1];
 			bot_direction[i][0] = Math.PI - bot_direction[i][0];
 		};
-		if (Math.abs(bot[i].position.z) >= ground_z / 2) {
+		if (Math.abs(bot[i].position.z) >= ground_z / 2.0) {
 			bot_direction[i][1] = -bot_direction[i][1];
 			bot_direction[i][0] = -bot_direction[i][0];
 		};
-
 	}
 
-	////ランダムな色の作成
-	//function randcolor() {
-	//	return '#' + Math.floor(Math.random() * 1677215).toString(16);
-	//}
+	//ランダムな色の作成
 	function randcolor() {
 		var color = (Math.random() * 0xFFFFFF | 0).toString(16);
 		return "#" + ("000000" + color).slice(-6);
+	}
+
+	//ボット作成（smokeParticlesに煙１つ１つを突っ込んでいる）
+	function MakeBot(basecolor = randcolor()) {
+		var smokeParticles = new THREE.Group();
+		for (var i = 0; i < smoke_num; i++) {
+			//単色
+			var smokeMaterial = new THREE.SpriteMaterial({ color: basecolor, map: smokeTexture, transparent: true, opacity: smoke_opacity });
+			////混色
+			//var smokeMaterial = new THREE.SpriteMaterial({ color: randcolor(), map: smokeTexture, transparent: true, opacity: 0.2 });
+			var sphere = new THREE.Sprite(smokeMaterial);
+			var z = (Math.random() - 0.5) * Math.PI;
+			var th = Math.random() * 2 * Math.PI;
+			var r = Math.random() * bot_rad;
+			sphere.position.set(r * Math.sin(z) * Math.cos(th), r * Math.sin(z) * Math.sin(th), r * Math.cos(z))
+			smokeParticles.add(sphere);
+		}
+		var x = (Math.random() - 0.5) * ground_x;
+		z = (Math.random() - 0.5) * ground_z;
+		smokeParticles.position.set(x, 0, z);
+
+		scene.add(smokeParticles);
+		bot.push(smokeParticles);
+		bot_direction.push([0, 0, Math.PI / 2]);
+	}
+
+	//画面いっぱいに#mainを表示するための関数
+	function setMain() {
+		//画面の幅と高さを取得
+		W = $("#main").width();
+		H = $(window).height();
+		//#mainに設定
+		$("#main").height(H);
+		//3D空間の大きさを画面の幅と高さに設定
+		renderer.setSize(W, H);
+		if (camera) {
+			//カメラの画角も画面サイズにあったものに
+			camera.aspect = W / H;
+			camera.updateProjectionMatrix();
+		}
 	}
 
 
