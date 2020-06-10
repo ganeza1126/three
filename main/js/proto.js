0fba05861ced69de6eb47d59d47591ae7cc2d078
ganeza1126 <ganeza1126@g.ecc.u-tokyo.ac.jp>
2020-06-11 02:45:31 +0900

    swap color not using bounding sphere

--- main/js/proto.js
+++ main/js/proto.js
@@ -18,44 +18,45 @@
 	var bot = [];
 	var bot_direction = [];
 	const bot_directionchange_freq = 150;
-	const bot_speed = 0.01;
+	//const bot_speed = 0.01;
+	const bot_speed = 0.005;
 	const ground_x = 8;
 	const ground_y = 1;
 	const ground_z = 8;
-	const bot_rad = 1;//半径
+	const bot_rad = 0.5;//半径
 	const smoke_num = 200;
 	const bot_num = 3;
 	const smoke_speed = 0.01;// 微小変化量（煙の）
 	var cnt = 0;
+	var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x000000, transparent: true, opacity: 0 });
+
+
+	var bot_pos = [];//ボット間の距離を測る
 
 	$(function () {
 		//-------------------------------------------------------
-		//画面いっぱいに#mainを表示するための関数
 		setMain();
-		//縦位置横位置が変わっても画面いっぱいに#mainを表示するための処理
 		$(window).on("orientationchange resize", setMain);
-
-		//#mainの中に3D空間用の<camvas>を作成
 		document.querySelector("#main").appendChild(renderer.domElement);
-
 		camera = new THREE.PerspectiveCamera(90, W / H, 1, 3000);
 		camera.position.set(0, 0, 10);
 		controls = new THREE.OrbitControls(camera, renderer.domElement);
 		controls.enableKeys = true;
-		//-------------------------------------------------------
-
-
-		for (var i = 0; i < bot_num; i++) {
-			MakeBot();
-		}
-
-		//newend
 		walls = new THREE.Mesh(new THREE.BoxGeometry(ground_x, ground_y, ground_z), new THREE.MeshBasicMaterial({ wireframe: true, color: 0xaaaaaa }));
 		axes = new THREE.AxesHelper(1000);
 		frame = new THREE.Group();
 		frame.add(walls);
 		frame.add(axes);
 		scene.add(frame);
+		//-------------------------------------------------------
+
+		for (var i = 0; i < bot_num; i++) {
+			MakeBot();
+		}
+		var botid = bot.length;
+
+
+		//			console.log(bot[botid].children[bot_num].geometry.BoundingSphere);
 
 		update();
 
@@ -79,13 +80,32 @@
 
 	//繰り返し実行するupdate()
 	function update() {
-
 		var botid = bot.length;
 		while (botid--) {
 			SmokeMov(bot[botid]);
 			BotMov(botid);
+			bot_pos[botid] = [bot[botid].position.x, bot[botid].position.z]
 		}
+		var i = bot_num;
+		while (i--) {
+			var j = i;
+			while (j--) {
+				var x = bot_pos[i];
+				var y = bot_pos[j];
+				if (Math.pow(x[0] - y[0], 2) + Math.pow(x[1] - y[1], 2) <= bot_rad * bot_rad) {
+					var colorid = Math.floor(Math.random() * smoke_num);
+					var tmp = bot[i].children[colorid].material.color;
+					//console.log("hit!!!!!!!!", tmp);
+					bot[i].children[colorid].material.color = bot[j].children[colorid].material.color;
+					bot[j].children[colorid].material.color = tmp;
+				}
+			}
+		}
+		//console.log(bot_pos)
 
+		//add bounding sphereを使う場合
+		//console.log(bot[0].children[smoke_num].geometry.boundingSphere.intersectsSphere(bot[1].children[smoke_num].geometry.boundingSphere));
+		//add
 		// botmov用カウンター更新
 		if (cnt == bot_directionchange_freq) {
 			cnt = 0;
@@ -93,28 +113,44 @@
 			cnt += 1;
 		};
 
+		//-------------------------------------------------------
 		controls.update();
-		//更新されたシーンとカメラ情報で、3D空間を再構築する
 		renderer.render(scene, camera);
-		//一定の間隔で再びupdate()を実行（これにより何度も繰り返しupdate()を実行する）
 		requestAnimationFrame(update);
+		//-------------------------------------------------------
 	}
 
-	function MakeBot() {
+
+	//function MakeBot(basecolor = randcolor()) {
+	function MakeBot(basecolor = randcolor()) {
 		var smokeParticles = new THREE.Group();
 		for (var i = 0; i < smoke_num; i++) {
-			var smokeMaterial = new THREE.SpriteMaterial({ color: randcolor(), map: smokeTexture, transparent: true, opacity: 0.2 });
+			//単色
+			var smokeMaterial = new THREE.SpriteMaterial({ color: basecolor, map: smokeTexture, transparent: true, opacity: 0.2 });
+			//混色
+			//var smokeMaterial = new THREE.SpriteMaterial({ color: randcolor(), map: smokeTexture, transparent: true, opacity: 0.2 });
 			var sphere = new THREE.Sprite(smokeMaterial);
 			var z = (Math.random() - 0.5) * Math.PI;
 			var th = Math.random() * 2 * Math.PI;
 			var r = Math.random() * bot_rad;
 			sphere.position.set(r * Math.sin(z) * Math.cos(th), r * Math.sin(z) * Math.sin(th), r * Math.cos(z))
-			//よくわからん
 			smokeParticles.add(sphere);
 		}
+		//boundingsphereを使う場合
+		////add
+		//var sphere = new THREE.Mesh(new THREE.SphereGeometry(bot_rad), sphereMaterial)
+		//sphere.geometry.computeBoundingSphere();
+		//smokeParticles.add(sphere);
+		////add
+
+		//otamesi
+		//console.log(smokeParticles.children[0].material.color);
+		//otamesi
+
 		scene.add(smokeParticles);
 		bot.push(smokeParticles);
 		bot_direction.push([0, 0, Math.PI / 2]);
+
 	}
 
 	//煙を動かす
@@ -161,9 +197,13 @@
 
 	}
 
-	//ランダムな色の作成
+	////ランダムな色の作成
+	//function randcolor() {
+	//	return '#' + Math.floor(Math.random() * 1677215).toString(16);
+	//}
 	function randcolor() {
-		return '#' + Math.floor(Math.random() * 1677215).toString(16);
+		var color = (Math.random() * 0xFFFFFF | 0).toString(16);
+		return "#" + ("000000" + color).slice(-6);
 	}
 
 
